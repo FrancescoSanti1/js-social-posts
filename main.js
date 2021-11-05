7a5a4f0339d6c3731d61321cae66678680bde69c
francesco <francescosanti91@gmail.com>
2021-11-05 14:47:02 +0100

    passi logici

---
+++ main.js
@@ -0,0 +1,12 @@
+// Passi logici
+
+// Creo un array di oggetti
+    // ogni oggetto contiene tutte le informazioni di un post
+// con un ciclo for attraverso l'array di oggetti
+    // se l'oggetto selezionato ha una stringa vuota nella proprietà immagine, 
+        // allora stampo una struttura html senza tag img
+        // altrimenti stampo la struttura html completa
+// al click su "mi piace"
+    // assegno al pulsante la classe ".like-button--liked"
+    // incremento di uno il numero di like
+    // aggiorno il numero di like visualizzato in pagina
